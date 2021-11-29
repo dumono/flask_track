9b8d12d046f8dbec31d5cd0c4784e36ff49f4fcf
dumon <dumono@gmail.com>
2021-11-29 09:59:11 +0300

    первые упражнения по flask сделаны

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from weather import weather_by_city
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ('Hello!')
+    weather = weather_by_city("Moscow, Russia")
+    weather = False
+    if weather:
+        return f"Сейчас {weather['temp_C']}," \
+           f"ощущается как {weather['FeelsLikeC']}"
+    else:
+        return "Прогноз сейчас недоступен"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug =True)
