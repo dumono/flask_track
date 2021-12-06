4ff53ff68aa9dfcbc1abd11dca013d3a1f889287
dumon <dumono@gmail.com>
2021-12-06 20:17:25 +0300

    трек flask 4 неделя

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
+from python_org_news import get_python_news
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = "Новости python"
     weather = weather_by_city("Moscow, Russia")
-    weather = False
-    if weather:
-        return f"Сейчас {weather['temp_C']}," \
-           f"ощущается как {weather['FeelsLikeC']}"
-    else:
-        return "Прогноз сейчас недоступен"
+    news_list = get_python_news()
+    return render_template('index.html',page_title=title, weather=weather, news_list=news_list)
 
 
 if __name__ == "__main__":
