ae73fd7aa0dd8a85d7da7874219e03f03f065ea3
dumon <dumono@gmail.com>
2021-12-13 09:21:49 +0300

    трек flask 5 неделя

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+from webapp.model import db, News
 
 def get_html(url):
     try:
@@ -9,6 +11,13 @@ def get_html(url):
     except(requests.RequestException, ValueError):
         print('Сетевая ошибка')
         return False
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
 
 def get_python_news():
     html = get_html("https://www.python.org/blogs/")
@@ -20,10 +29,15 @@ def get_python_news():
             title = news.find('a').text
             url = news.find('a')['href']
             published = news.find('time').text
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except(ValueError):
+                published = datetime.now()
             rsult_news.append({
                 "title": title,
                 "url": url,
                 "published": published
             })
-        return rsult_news
+        #return rsult_news
+            save_news(title=title, url=url, published=published)
     return False
--- webapp/weather.py
+++ webapp/weather.py
@@ -1,10 +1,11 @@
 import requests
 import settings
+from flask import current_app
 
 def weather_by_city(city_name):
     weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
     params = {
-        "key" : settings.API_KEY,
+        "key": current_app.config['WEATHER_API_KEY'],
         "q" : city_name,
         "format" : "json",
         "num_of_days" : 1,
